52832207ddc4d41cab2affc50a7b2eb4d2cc4f85
sergio981 <69440338+Sergio-Bautista@users.noreply.github.com>
2026-06-16 19:04:01 -0500

    added delete route

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -8,4 +8,5 @@ const { route } = require('./indexRoute');
 router.get('/', ensureAuth, dashboardController.getDashboard);
 router.get('/expenseForm', dashboardController.expenseForm)
 router.post('/addNewExpense', dashboardController.addNewExpense)
+router.post('/delete/:id', ensureAuth, dashboardController.deleteExpense)
 module.exports = router;
