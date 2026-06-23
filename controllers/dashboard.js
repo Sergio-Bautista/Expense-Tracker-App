25b9f131119d704e993e931d081a5b244346ab6a
sergio981 <69440338+Sergio-Bautista@users.noreply.github.com>
2026-06-22 21:04:53 -0500

    working on budget function

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -8,12 +8,15 @@ module.exports = {
         console.log(req.user);
         try{
             const userId = req.user._id;
-
-            const expenses = await Expense.find({ userId }).sort({ date:-1 })
-
             const now = new Date();
             const currentMonth = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}`;
 
+            const expenses = await Expense.find({ userId }).sort({ date:-1 })
+            // const budgets = await Budget.find({ userId })
+            const budgets = await Budget.find({
+                userId, 
+                month: currentMonth
+            })
             const currentMonthExpenses = expenses.filter(e =>{
                 const expenseMonth = `${e.date.getFullYear()}-${String(e.date.getMonth() + 1).padStart(2, '0')}`;
                 return expenseMonth === currentMonth
@@ -24,18 +27,60 @@ module.exports = {
               categoryTotals[expense.category] = (categoryTotals[expense.category] || 0) + expense.amount;
             });
 
-
+            
             const totalSpent = currentMonthExpenses.reduce((sum, acc) => sum + acc.amount, 0);
+            // const totalBudget= budgets.reduce((sum, acc) => sum + acc.limit, 0);
+            
+            if(budgets){
+               const budgetCategory = {};
+               budgets.forEach(b => {
+                budgetCategory[b.category] = (budgetCategory[b.category] || 0 ) + b.limit
+               })
+               
+               const budgetCategoryTotals = {}
 
-            const budgets = await Budget.find({ userId, month: currentMonth });
+                for (const category in budgetCategory) {
+                    const spent = budgetCategory[category];
+                
+                    const limit = budgets
+                        .filter(budget => budget.category === category)
+                        .reduce((sum, budget) => sum + budget.limit, 0);
+                
+                    const remaining = limit - spent;
+                    budgetCategoryTotals[category] = remaining
 
-            res.render('dashboard.ejs', {
-                expenses: currentMonthExpenses,
-                totalSpent, 
-                categoryTotals, 
-                budgets, 
-                currentMonth
-            });
+                    // console.log("Category:", category);
+                    // console.log("Spent:", spent);
+                    // console.log("Limit:", limit);
+                    // console.log("Remaining:", remaining);
+                    console.log(categoryTotals);
+                    console.log(budgetCategory);
+                }
+                return res.render('dashboard.ejs', {
+                    expenses: currentMonthExpenses,
+                    totalSpent, 
+                    categoryTotals, 
+                    budgets, 
+                    budgetCategoryTotals,
+                    // totalBudget,
+                    currentMonth,
+                    // budgetMoneyLeft
+                });
+
+            }else{
+                return res.render('dashboard.ejs', {
+                    expenses: currentMonthExpenses,
+                    totalSpent,
+                    categoryTotals,
+                    budget: null,
+                    totalBudget: 0,
+                    budgetMoneyLeft: 0,
+                    currentMonth
+                });
+
+            }
+
+
         }catch(err){
             console.error(err);
             res.status(500).send(err.message)
@@ -45,6 +90,16 @@ module.exports = {
     expenseForm: (req, res) =>{
         res.render('expenseForm.ejs')
     }, 
+    editExpenseForm: async (req, res) =>{
+        const id = req.params.id
+        const expense = await Expense.findById(id)
+
+        if(!expense){
+            return res.status(404).send("Expense not found!")
+        }
+
+        res.render('editExpenseForm.ejs', {expense})
+    },
     addNewExpense: async(req, res)=>{
         try{
             const { description, amount, category, date } = req.body;
@@ -59,16 +114,13 @@ module.exports = {
                 res.redirect('/dashboard');
         }catch(err){
             res.status(500).send(err.message);
-            }
+        }
     },
     deleteExpense: async (req, res)=>{
-        console.log("USER:", req.user);
-        console.log("PARAMS:", req.params);
         try{
             if(!req.user){
                 return res.redirect('/login')
             }
-
             const id = req.params.id
             const expense = Expense.findById(id)
 
@@ -84,5 +136,52 @@ module.exports = {
             console.log(err);
             res.redirect('/dashboard')
         }
+    }, 
+    updateExpense: async (req, res) =>{
+        try{
+            if(!req.user){
+                return res.redirect('/login')
+            }
+            const id = req.params.id;
+            const expense = Expense.findById(id)
+            
+            if(!expense){
+                return res.status(404).send("Expense not found")
+            }
+
+            const { description, amount, category, date } = req.body;
+
+            await Expense.findByIdAndUpdate(id, {
+                description, 
+                amount, 
+                category, 
+                date: date || new Date()
+            });
+            res.redirect('/dashboard')
+
+        }catch(err){
+            res.status(500).send(err.message)
+            res.redirect('/dashboard')
+        }
+    }, 
+    addNewBudget: async(req, res)=>{
+        try{
+            const { limit, category, month } = req.body;
+            const budget = new Budget({
+                userId: req.user._id,
+                limit, 
+                category, 
+                month, 
+            });
+            await budget.save();
+            res.redirect('/dashboard');
+        }catch(err){
+            res.status(500).send(err.message)
+        }
+
+    },
+    budgetForm: (req, res) =>{
+        res.render('budgetForm.ejs')
     }
+    
 }
